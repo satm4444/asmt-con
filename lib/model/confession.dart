8315727bf9577449abb5cb7c55906e28de19da4a
Satyam <rawalsatyam018@gmail.com>
2020-08-08 22:48:44 +0545

    second

--- lib/model/confession.dart
+++ lib/model/confession.dart
@@ -4,14 +4,12 @@ import 'package:flutter/cupertino.dart';
 class Confession with ChangeNotifier {
   final String id;
   final String sem;
-  final String colorcode;
   final String faculty;
   final String description;
   final List<Comment> comment;
   Confession(
       {@required this.id,
       @required this.sem,
-      this.colorcode,
       @required this.faculty,
       @required this.description,
       this.comment});
